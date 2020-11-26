117729757ece44a68958048944ec3ab55a7ad62d
kocherovets <kocherovets@gmail.com>
2020-11-26 20:35:53 +0300

    2.0.1

--- Package.swift
+++ Package.swift
@@ -16,7 +16,7 @@ let package = Package(
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
         .package(url: "https://github.com/kocherovets/DeclarativeTVC", from: "1.0.22"),
-        .package(url: "https://github.com/kocherovets/RedSwift", from: "1.0.31"),
+        .package(url: "https://github.com/kocherovets/RedSwift", from: "2.0.1"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
